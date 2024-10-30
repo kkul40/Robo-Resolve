1bfe8c6f04e5c1b71c3fd9e895de98c0a6d14727
Kadir Kul <56093519+kkul40@users.noreply.github.com>
2024-10-30 17:55:02 -0400

    Added - Wall Jump Mechanic (#4)
    Battery Indicator Added
    and some adjustments

--- Assets/_Project/RGScripts/_Refactored/Player/PlayerConfig.cs
+++ Assets/_Project/RGScripts/_Refactored/Player/PlayerConfig.cs
@@ -6,14 +6,23 @@ namespace _Project.RGScripts.Player
     [CreateAssetMenu(fileName = "Player Config", menuName = "Player", order = 0)]
     public class PlayerConfig : ScriptableObject
     {
+        [Header("Player Configs")]
         public float DefaultGravityScale = 6;
         public float FallingGravityScale = 8.5f;
         public float RayDetectionLenght = 0.1f;
+        [FormerlySerializedAs("DontHugWallAfterWallJumpTimer")] [FormerlySerializedAs("DontHugWallTimerAfterWallJump")] public float DontHugWallAfterWallJumpDelay = 0.4f;
         public LayerMask WhatIsGround;
-        public float AccelerationSpeed = 5;
+        
+        [Header("Player Settings")]
+        public float AccelerationSpeed = 10;
         public float DecelerationSpeed = 10;
+        
+        public float AirAccelerationSpeed = 5;
+        public float AirDecerationSpeed = 5;
+        
         public float RunSpeed = 5;
-        public float JumpForce = 10;
+        public float JumpForce = 20;
         public float WallSlideSpeed = 1;
+        public float MaxEnergy = 100;
     }
 }
---
+++ Assets/_Project/RGScripts/_Refactored/Player/PlayerStates/WallJumpState.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+
+namespace _Project.RGScripts.Player
+{
+    public class WallJumpState : PlayerState
+    {
+        private float timer = 0;
+        public WallJumpState(Player player, PlayerConfig settings) : base(player, settings)
+        {
+        }
+
+        public override void Enter()
+        {
+            WallJump();
+            _player.SetAnimation(PlayerStateType.Jump);
+            _player.FlipPlayer(!_player.IsFacingRight);
+
+            timer = 0;
+        }
+
+        public override void FrameUpdate()
+        {
+            _player.HandleHorizontalMovement(_settings.AirAccelerationSpeed, _settings.AirDecerationSpeed, _input.MovementInput);
+
+            timer += Time.deltaTime;
+            
+            if (_player.IsWalled() && timer >= _settings.DontHugWallAfterWallJumpDelay)
+            {
+                _stateMachine.ChangeState(PlayerStateType.WallSlide);
+            }
+            else if (_player.Velocity.y <= 0)
+            {
+                _stateMachine.ChangeState(PlayerStateType.Fall);
+            }
+        }
+
+        private void WallJump()
+        {
+            _player.SetVelocity(0, 0);
+            Vector2 jumpDirection = Vector2.zero;
+
+            if (_player.IsFacingRight)
+            {
+                if (_input.MovementInput.x > 0)
+                {
+                    jumpDirection.x = -0.3f;
+                }
+                else
+                {
+                    jumpDirection.x = -1;
+                }
+            }
+            else
+            {
+                if (_input.MovementInput.x < 0)
+                {
+                    jumpDirection.x = 0.3f;
+                }
+                else
+                {
+                    jumpDirection.x = 1;
+                }
+            }
+            
+            jumpDirection.y = 1;
+            
+            _rigidbody2D.AddForce(jumpDirection * _settings.JumpForce, ForceMode2D.Impulse);
+        }
+
+        public override bool CanTransitionInto()
+        {
+            return true;
+        }
+    }
+}
